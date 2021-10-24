39e083c19998549224a6fbf8111156bb5065bb9a
Michael Heise <mh.mh@web.de>
2021-10-24 19:15:12 +0200

    apply isort/black/flake8 rules add MPD port configuration parameter allow more than one button and keep track of them in list implement rotary encoders replace 'pass' by 'sleep' in service loop *bug-fix* add missing self references for method calls *bug-fix* correct use of string join

--- raspi-gpio-mpdc.py
+++ raspi-gpio-mpdc.py
@@ -3,8 +3,8 @@
 __author__ = "Michael Heise"
 __copyright__ = "Copyright (C) 2021 by Michael Heise"
 __license__ = "Apache License Version 2.0"
-__version__ = "0.0.2"
-__date__ = "09/07/2021"
+__version__ = "0.0.3"
+__date__ = "10/24/2021"
 
 """MPD/Mopidy client which is controlled by GPIO Zero on Raspberry Pi
 """
@@ -25,18 +25,104 @@ __date__ = "09/07/2021"
 
 # standard imports
 import configparser
-import sys
-import weakref
-import signal
 import logging
+import signal
+import socket
+import sys
+import time
+import weakref
 
 # 3rd party imports
 import gpiozero
+import mpd
 from systemd.journal import JournalHandler
-from mpd import MPDClient
 
 # local imports
 # - none -
+
+
+class PersistentMPDClient(mpd.MPDClient):
+    def __init__(self, socket=None, host=None, port=None):
+        super(PersistentMPDClient, self).__init__()
+        self.socket = socket
+        self.host = host
+        self.port = port
+
+        self.do_connect()
+        # get list of available commands from client
+        self.command_list = self.commands()
+
+        # commands not to intercept
+        self.command_blacklist = ["ping"]
+
+        # wrap all valid MPDClient functions
+        # in a ping-connection-retry wrapper
+        for cmd in self.command_list:
+            if cmd not in self.command_blacklist:
+                if hasattr(super(PersistentMPDClient, self), cmd):
+                    super_fun = super(PersistentMPDClient, self).__getattribute__(cmd)
+                    new_fun = self.try_cmd(super_fun)
+                    # print("Setting interceptor for {}".format(cmd))
+                    setattr(self, cmd, new_fun)
+                else:
+                    print("Attr {} not available!".format(cmd))
+
+    # create a wrapper for a function (such as an MPDClient
+    # member function) that will verify a connection (and
+    # reconnect if necessary) before executing that function.
+    # functions wrapped in this way should always succeed
+    # (if the server is up)
+    # we ping first because we don't want to retry the same
+    # function if there's a failure, we want to use the noop
+    # to check connectivity
+    def try_cmd(self, cmd_fun):
+        def fun(*pargs, **kwargs):
+            try:
+                self.ping()
+            except (mpd.ConnectionError, OSError):
+                #                print("lost connection.")
+                #                print("trying to reconnect.")
+                self.do_connect()
+            return cmd_fun(*pargs, **kwargs)
+
+        return fun
+
+    # needs a name that does not collide with parent connect() function
+    def do_connect(self):
+        try:
+            try:
+                #                print("Attempting to disconnect.")
+                self.disconnect()
+            # if it's a TCP connection, we'll get a socket error
+            # if we try to disconnect when the connection is lost
+            except mpd.ConnectionError:
+                #                print("Disconnect failed, so what?")
+                pass
+            # if it's a socket connection, we'll get a BrokenPipeError
+            # if we try to disconnect when the connection is lost
+            # but we have to retry the disconnect, because we'll get
+            # an "Already connected" error if we don't.
+            # the second one should succeed.
+            except BrokenPipeError:
+                #                print("Pipe closed, retrying disconnect.")
+                try:
+                    #                    print("Retrying disconnect.")
+                    self.disconnect()
+                except Exception:
+                    #                    print("Second disconnect failed, yikes.")
+                    #                    print(e)
+                    pass
+            if self.socket:
+                #                print("Connecting to {}".format(self.socket))
+                self.connect(self.socket, None)
+            else:
+                #                print("Connecting to {}:{}".format(self.host, self.port))
+                self.connect(self.host, self.port)
+        except socket.error:
+            print("Connection refused.")
+
+
+#            print(e)
 
 
 class RaspiGPIOMPDClient:
@@ -66,12 +152,15 @@ class RaspiGPIOMPDClient:
 
     def __init__(self):
         self._finalizer = weakref.finalize(self, self.finalize)
+        self._buttons = []
+        self._rotencs = []
 
         self.isValidGPIO = False
+        self.isConnected = False
 
         self.config = None
         self.mpd = None
-        
+
         self.btn_held = {}
 
     def remove(self):
@@ -91,7 +180,8 @@ class RaspiGPIOMPDClient:
         logHandler = JournalHandler()
         logHandler.setFormatter(log_fmt)
         log.addHandler(logHandler)
-        log.setLevel(logging.INFO)
+        # log.setLevel(logging.INFO)
+        log.setLevel(logging.DEBUG)
         self._log = log
         self._log.info("Initialized logging.")
 
@@ -116,23 +206,39 @@ class RaspiGPIOMPDClient:
             self._log.info("Connect to MPD.")
             configMPD = self.config["MPD"]
 
-            if not configMPD:
+            if (
+                not configMPD
+                or "mpdhost" not in configMPD
+                or configMPD["mpdhost"] == ""
+            ):
                 host = "localhost"
             else:
                 host = configMPD["mpdhost"]
 
-            self.mpd = MPDClient()
-            self.mpd.timeout = 10
-            self.mpd.idletimeout = None
-            self.mpd.connect(host, 6600)
-            self.mpd.close()
-            self.mpd.disconnect()
+            if (
+                not configMPD
+                or "mpdport" not in configMPD
+                or configMPD["mpdport"] == ""
+            ):
+                port = 6600
+            else:
+                port = configMPD["mpdport"]
+
+            self._log.info(f"Host: {host}@{port}")
+
+            self.mpd = PersistentMPDClient(host=host, port=port)
+
+            self._log.debug(self.mpd.status())
+
+            self.isConnected = True
+            return True
         except Exception as e:
             self._log.error(f"Connection to MPD failed! ({e})")
+            return False
 
     def configButton(self, buttonConfig):
         """configure one button"""
-        pudMode = checkResistor(buttonConfig[1])
+        pudMode = self.checkResistor(buttonConfig[1])
         if pudMode == -1:
             return False
 
@@ -149,23 +255,23 @@ class RaspiGPIOMPDClient:
         event = True if eventStr == "press" else False
 
         triggered_event = buttonConfig[3]
-        if not checkTriggeredEvent(triggered_event):
+        if not self.checkTriggeredEvent(triggered_event):
             return False
 
         if len(buttonConfig) == 5:
             try:
                 bouncetime = int(buttonConfig[4])
-            except:
+            except Exception:
                 self._log.error("Invalid bounce time! (only integer >0 allowed)")
                 return False
         else:
             bouncetime = 100
 
-        return setupButton(
+        return self.setupButton(
             buttonConfig[0], pud, active, event, triggered_event, bouncetime
         )
 
-    def setupButton(self, pin, pull, active, event, triggered_event):
+    def setupButton(self, pin, pull, active, event, triggered_event, bouncetime):
         """setup GPIOZero object for button"""
         try:
             button = gpiozero.Button(
@@ -174,39 +280,104 @@ class RaspiGPIOMPDClient:
                 active_state=active,
                 bounce_time=0.001 * bouncetime,
             )
+
             event_func = getattr(self, triggered_event)
+
+            if not event_func:
+                raise ValueError("Could not determine event function!")
+
             if event:
                 button.when_pressed = event_func
             else:
                 button.when_released = event_func
+
+            self._buttons.append(button)
             return True
-        except Except as e:
-            self._log.error("Error while setting up GPIO input for button! ({e})")
+        except Exception as e:
+            self._log.error(f"Error while setting up GPIO input for button! ({e})")
             return False
 
     def configRotEnc(self, rotencConfig):
         """Configure one rotary encoder"""
-        pass
+        pudMode = self.checkResistor(rotencConfig[2])
+        if pudMode == -1:
+            return False
 
-    def setupRotEnc(self, pinA, pinB, pull, triggered_ccw_event, triggered_cw_event):
+        try:
+            pud, active = self.PUD_SWITCHER[pudMode]
+        except Exception as e:
+            self._log.error(f"Could not convert pull resistor configuration! ({e})")
+            return False
+
+        triggered_event_ccw = rotencConfig[3]
+        if not self.checkTriggeredEvent(triggered_event_ccw):
+            return False
+
+        triggered_event_cw = rotencConfig[4]
+        if not self.checkTriggeredEvent(triggered_event_cw):
+            return False
+
+        if len(rotencConfig) == 6:
+            try:
+                bouncetime = int(rotencConfig[5])
+            except Exception:
+                self._log.error("Invalid bounce time! (only integer >0 allowed)")
+                return False
+        else:
+            bouncetime = 100
+
+        return self.setupRotEnc(
+            rotencConfig[0],
+            rotencConfig[1],
+            pud,
+            triggered_event_ccw,
+            triggered_event_cw,
+            bouncetime,
+        )
+
+    def setupRotEnc(
+        self, pinA, pinB, pull, triggered_ccw_event, triggered_cw_event, bouncetime
+    ):
         """Setup GPIOZero rotary encoder object"""
-        pass
+        try:
+            rotenc = gpiozero.RotaryEncoder(
+                int(pinA), int(pinB), bounce_time=0.001 * bouncetime, max_steps=50
+            )
+
+            event_func_ccw = getattr(self, triggered_ccw_event)
+            if not event_func_ccw:
+                raise ValueError("Could not determine event function!")
+
+            event_func_cw = getattr(self, triggered_cw_event)
+            if not event_func_cw:
+                raise ValueError("Could not determine event function!")
+
+            rotenc.when_rotated_counter_clockwise = event_func_ccw
+            rotenc.when_rotated_clockwise = event_func_cw
+
+            self._rotencs.append(rotenc)
+            return True
+        except Exception as e:
+            self._log.error(
+                f"Error while setting up GPIO input for rotary encoder! ({e})"
+            )
+            return False
 
     def checkResistor(self, pudStr):
         """Return index if string is found in pre-defined values for pull resistor types"""
         try:
-            return self.VALUES_PULLUPDNUPDN.index(pudStr)
-        except:
+            return self.VALUES_PULLUPDN.index(pudStr)
+        except Exception:
             self._log.error(
                 "Invalid resistor configuration! Only one of {0} allowed!".format(
-                    self.VALUES_PULLUPDNUPDN.join("/")
+                    "/".join(self.VALUES_PULLUPDN)
                 )
             )
             return -1
 
     def checkButtonEvent(self, buttonEvent):
         """Return if string is in pre-defined values for button events"""
-        if not buttonEvent in self.VALUES_PRESSRELEASE:
+        if buttonEvent not in self.VALUES_PRESSRELEASE:
             self._log.error(
                 "Invalid event configuration! Only 'PRESS' or 'RELEASE' allowed!"
             )
@@ -215,10 +386,10 @@ class RaspiGPIOMPDClient:
 
     def checkTriggeredEvent(self, triggeredEvent):
         """Return true if string..."""
-        if not triggeredEvent in self.VALUES_TRIGGERED_EVENTS:
+        if triggeredEvent not in self.VALUES_TRIGGERED_EVENTS:
             self._log.error(
                 "Invalid event! Only one of {0} allowed!".format(
-                    self.VALUES_TRIGGERED_EVENTS.join("/")
+                    "/".join(self.VALUES_TRIGGERED_EVENTS)
                 )
             )
             return False
@@ -231,14 +402,14 @@ class RaspiGPIOMPDClient:
 
         self._vol_step = 5
 
-        for key, value in configGPIO:
-            if key.starts_with("Button"):
+        for key, value in configGPIO.items():
+            if key.startswith("button"):
                 self._log.info(f"Button configuration '{key} = {value}'")
                 if not self.configButton(value.lower().split(",")):
                     return False
                 continue
 
-            if key.starts_with("RotEnc"):
+            if key.startswith("rotenc"):
                 self._log.info(f"RotEnc configuration '{key} = {value}'")
                 if not self.configRotEnc(value.lower().split(",")):
                     return False
@@ -254,39 +425,66 @@ class RaspiGPIOMPDClient:
 
     def none(self):
         pass
-    
+
     def play_pause(self):
-        if self.mpd.status()["state"] == "play":
+        self._log.debug("play_pause: ")
+        prev_state = self.mpd.status()["state"]
+        if prev_state == "play":
             self.mpd.pause()
         else:
             self.mpd.play()
+        self._log.debug(prev_state + "->" + self.mpd.status()["state"])
 
     def play_stop(self):
-        if self.mpd.status()["state"] == "play":
+        self._log.debug("play_stop: ")
+        prev_state = self.mpd.status()["state"]
+        if prev_state == "play":
             self.mpd.stop()
         else:
             self.mpd.play()
+        self._log.debug(prev_state + "->" + self.mpd.status()["state"])
 
     def prev_track(self):
+        self._log.debug("prev_track")
         self.mpd.previous()
+        self._log.debug(self.mpd.status())
 
     def next_track(self):
+        self._log.debug("next_track")
         self.mpd.next()
+        self._log.debug(self.mpd.status())
 
     def mute(self):
+        self._log.debug("mute")
         self.mpd.toggleoutput(0)
+        self._log.debug(self.mpd.status())
 
     def vol_dn(self):
-        self.mpd.volume(-self._vol_step)
+        self._log.debug("vol_dn")
+        prev_vol = int(self.mpd.status()["volume"])
+        if prev_vol > 0:
+            if prev_vol >= self._vol_step:
+                self.mpd.volume(-self._vol_step)
+            else:
+                self.mpd.volume(0)
+            self._log.debug(str(prev_vol) + "->" + str(self.mpd.status()["volume"]))
 
     def vol_up(self):
-        self.mpd.volue(+self._vol_step)
+        self._log.debug("vol_up")
+        prev_vol = int(self.mpd.status()["volume"])
+        if prev_vol < 100:
+            if prev_vol + self._vol_step <= 100:
+                self.mpd.volume(+self._vol_step)
+            else:
+                self.mpd.volume(100)
+            self._log.debug(str(prev_vol) + "->" + str(self.mpd.status()["volume"]))
 
     def prev_src(self):
         pass
-    
+
     def next_src(self):
         pass
+
 
 def sigterm_handler(_signo, _stack_frame):
     """clean exit on SIGTERM signal (when systemd stops the process)"""
@@ -314,21 +512,22 @@ try:
 
     if not mpdclient.initMPD():
         log.error("Init MPD connection failed!")
+        sys.exit(-4)
 
     if not mpdclient.initGPIO():
         log.error("Init GPIO failed!")
         sys.exit(-3)
 
-    log.info("Enter service loop...")
+    log.info("Enter raspi-gpio-mpdc service loop...")
     while True:
-        pass
+        time.sleep(1)
 
 except Exception as e:
     if log:
-        log.exception("Unhandled exception: {0}".format(e.args[0]))
+        log.exception(f"Unhandled exception: {e}")
     sys.exit(-1)
 finally:
     if mpdclient and mpdclient.isConnected:
         if log:
             log.info("Disconnect from MPD.")
-        mpdclient.disconnect()
+        mpdclient.mpd.disconnect()
