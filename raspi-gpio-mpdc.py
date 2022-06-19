d00db2ed6d95eae8354f5e9119ea61663987870b
Michael Heise <mh.mh@web.de>
2022-06-19 09:54:23 +0200

    extend logging to PersistentMPDClient add inline documentation apply flake8 formatting create main method (script-only) connectMPD function to repeatedly try connect reduce service loop delay to 100ms for quicker response

--- raspi-gpio-mpdc.py
+++ raspi-gpio-mpdc.py
@@ -4,12 +4,12 @@ __author__ = "Michael Heise"
 __copyright__ = "Copyright (C) 2021 by Michael Heise"
 __license__ = "Apache License Version 2.0"
 __version__ = "0.0.3"
-__date__ = "10/24/2021"
+__date__ = "06/19/2022"
 
 """MPD/Mopidy client which is controlled by GPIO Zero on Raspberry Pi
 """
 
-#    Copyright 2021 Michael Heise (mikiair)
+#    Copyright 2022 Michael Heise (mikiair)
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -42,30 +42,49 @@ from systemd.journal import JournalHandler
 
 
 class PersistentMPDClient(mpd.MPDClient):
-    def __init__(self, socket=None, host=None, port=None):
+    def __init__(self, socket=None, host=None, port=None, log=None):
         super(PersistentMPDClient, self).__init__()
         self.socket = socket
         self.host = host
         self.port = port
+        self.log = log
 
-        self.do_connect()
-        # get list of available commands from client
-        self.command_list = self.commands()
-
-        # commands not to intercept
         self.command_blacklist = ["ping"]
 
-        # wrap all valid MPDClient functions
-        # in a ping-connection-retry wrapper
-        for cmd in self.command_list:
-            if cmd not in self.command_blacklist:
-                if hasattr(super(PersistentMPDClient, self), cmd):
-                    super_fun = super(PersistentMPDClient, self).__getattribute__(cmd)
-                    new_fun = self.try_cmd(super_fun)
-                    # print("Setting interceptor for {}".format(cmd))
-                    setattr(self, cmd, new_fun)
-                else:
-                    print("Attr {} not available!".format(cmd))
+        self.connection_established = False
+        self.establish_connection()
+
+    def establish_connection(self):
+        """Establish the connection to the MPD server by trying to connect,
+        and if not yet connected before (= not established),
+        then read out the command list and dereference to auto-connect commands
+        """
+        try:
+            self.do_connect()
+
+            if not self.connection_established:
+                # get list of available commands from client
+                command_list = self.commands()
+
+                # wrap all valid MPDClient functions
+                # in a ping-connection-retry wrapper
+                for cmd in command_list:
+                    if cmd not in self.command_blacklist:
+                        if hasattr(super(PersistentMPDClient, self), cmd):
+                            super_func = super(
+                                PersistentMPDClient, self
+                            ).__getattribute__(cmd)
+                            new_func = self.try_cmd(super_func)
+                            setattr(self, cmd, new_func)
+                        else:
+                            if self.log is not None:
+                                self.log.debug("Unknown command attribute '{cmd}'!")
+                            pass
+
+                self.connection_established = True
+        except Exception as e:
+            if self.log is not None:
+                self.log.error(f"Error when establishing connection to MPD server: {e}")
 
     # create a wrapper for a function (such as an MPDClient
     # member function) that will verify a connection (and
@@ -75,28 +94,30 @@ class PersistentMPDClient(mpd.MPDClient):
     # we ping first because we don't want to retry the same
     # function if there's a failure, we want to use the noop
     # to check connectivity
-    def try_cmd(self, cmd_fun):
-        def fun(*pargs, **kwargs):
+
+    def try_cmd(self, cmd_func):
+        """Wrapper function which pings the MPD server and
+        in case of failure tries to re-connect before carrying out the actual command.
+        """
+
+        def func(*pargs, **kwargs):
             try:
                 self.ping()
             except (mpd.ConnectionError, OSError):
-                #                print("lost connection.")
-                #                print("trying to reconnect.")
                 self.do_connect()
-            return cmd_fun(*pargs, **kwargs)
+            return cmd_func(*pargs, **kwargs)
 
-        return fun
+        return func
 
     # needs a name that does not collide with parent connect() function
     def do_connect(self):
         try:
             try:
-                #                print("Attempting to disconnect.")
+                # Attempting to disconnect
                 self.disconnect()
             # if it's a TCP connection, we'll get a socket error
             # if we try to disconnect when the connection is lost
             except mpd.ConnectionError:
-                #                print("Disconnect failed, so what?")
                 pass
             # if it's a socket connection, we'll get a BrokenPipeError
             # if we try to disconnect when the connection is lost
@@ -104,25 +125,18 @@ class PersistentMPDClient(mpd.MPDClient):
             # an "Already connected" error if we don't.
             # the second one should succeed.
             except BrokenPipeError:
-                #                print("Pipe closed, retrying disconnect.")
                 try:
-                    #                    print("Retrying disconnect.")
                     self.disconnect()
                 except Exception:
-                    #                    print("Second disconnect failed, yikes.")
-                    #                    print(e)
                     pass
+
             if self.socket:
-                #                print("Connecting to {}".format(self.socket))
                 self.connect(self.socket, None)
             else:
-                #                print("Connecting to {}:{}".format(self.host, self.port))
                 self.connect(self.host, self.port)
         except socket.error:
-            print("Connection refused.")
-
-
-#            print(e)
+            if self.log is not None:
+                self.log.error("MPD Server: connection refused!")
 
 
 class RaspiGPIOMPDClient:
@@ -171,11 +185,11 @@ class RaspiGPIOMPDClient:
         return not self._finalizer.alive
 
     def finalize(self):
-        if self.mpd:
+        if self.mpd is not None:
             self.mpd.disconnect()
 
     def initLogging(self, log):
-        """initialize logging to journal"""
+        """Initialize logging to journal"""
         log_fmt = logging.Formatter("%(levelname)s %(message)s")
         logHandler = JournalHandler()
         logHandler.setFormatter(log_fmt)
@@ -187,10 +201,9 @@ class RaspiGPIOMPDClient:
 
         pinf = type(gpiozero.Device._default_pin_factory()).__name__
         self._log.info(f"GPIO Zero default pin factory: {pinf}")
-        return
 
     def readConfigFile(self):
-        """read the config file"""
+        """Read the config file"""
         try:
             self._log.info(f"Reading configuration file... '{self.CONFIGFILE}")
             self.config = configparser.ConfigParser()
@@ -201,9 +214,8 @@ class RaspiGPIOMPDClient:
             return False
 
     def initMPD(self):
-        """establish the connection to MPD server"""
+        """Initialize the connection to MPD server"""
         try:
-            self._log.info("Connect to MPD.")
             configMPD = self.config["MPD"]
 
             if (
@@ -224,17 +236,42 @@ class RaspiGPIOMPDClient:
             else:
                 port = configMPD["mpdport"]
 
-            self._log.info(f"Host: {host}@{port}")
+            self._log.info(f"Initialize connection to MPD server: {host}:{port}")
 
-            self.mpd = PersistentMPDClient(host=host, port=port)
+            self.mpd = PersistentMPDClient(host=host, port=port, log=self._log)
 
-            self._log.debug(self.mpd.status())
+            self.isConnected = self.mpd.connection_established
 
-            self.isConnected = True
+            if self.isConnected:
+                self._log.debug(self.mpd.status())
+
             return True
         except Exception as e:
             self._log.error(f"Connection to MPD failed! ({e})")
             return False
+
+    def connectMPD(self):
+        """Try connecting to MPD server up to 50 times (10 seconds)
+        to establish auto-reconnect commands.
+        """
+        if not self.mpd:
+            return False
+
+        self._log.info("Wait until MPD server started and try establishing connection.")
+        num_tried = 0
+        while not self.isConnected and num_tried < 50:
+            try:
+                self.mpd.establish_connection()
+                self.isConnected = self.mpd.connection_established
+                if self.isConnected:
+                    self._log.debug(self.mpd.status())
+                    return True
+            except Exception:
+                pass
+            num_tried += 1
+            time.sleep(200)
+
+        return False
 
     def configButton(self, buttonConfig):
         """configure one button"""
@@ -272,7 +309,7 @@ class RaspiGPIOMPDClient:
         )
 
     def setupButton(self, pin, pull, active, event, triggered_event, bouncetime):
-        """setup GPIOZero object for button"""
+        """Setup GPIOZero object for button"""
         try:
             button = gpiozero.Button(
                 int(pin),
@@ -423,9 +460,6 @@ class RaspiGPIOMPDClient:
 
     # trigger event handlers
 
-    def none(self):
-        pass
-
     def play_pause(self):
         self._log.debug("play_pause: ")
         prev_state = self.mpd.status()["state"]
@@ -463,21 +497,25 @@ class RaspiGPIOMPDClient:
         self._log.debug("vol_dn")
         prev_vol = int(self.mpd.status()["volume"])
         if prev_vol > 0:
-            if prev_vol >= self._vol_step:
+            if prev_vol > self._vol_step:
                 self.mpd.volume(-self._vol_step)
             else:
                 self.mpd.volume(0)
             self._log.debug(str(prev_vol) + "->" + str(self.mpd.status()["volume"]))
+        else:
+            self._log.debug(str(prev_vol))
 
     def vol_up(self):
         self._log.debug("vol_up")
         prev_vol = int(self.mpd.status()["volume"])
         if prev_vol < 100:
-            if prev_vol + self._vol_step <= 100:
+            if prev_vol + self._vol_step < 100:
                 self.mpd.volume(+self._vol_step)
             else:
                 self.mpd.volume(100)
             self._log.debug(str(prev_vol) + "->" + str(self.mpd.status()["volume"]))
+        else:
+            self._log.debug(str(prev_vol))
 
     def prev_src(self):
         pass
@@ -491,43 +529,56 @@ def sigterm_handler(_signo, _stack_frame):
     sys.exit(0)
 
 
-# install handler
-signal.signal(signal.SIGTERM, sigterm_handler)
+def main():
+    # install handler
+    signal.signal(signal.SIGTERM, sigterm_handler)
 
-log = None
-mpdclient = None
+    log = None
+    mpdclient = None
 
-try:
-    log = logging.getLogger(__name__)
+    try:
+        log = logging.getLogger(__name__)
 
-    mpdclient = RaspiGPIOMPDClient()
-    mpdclient.initLogging(log)
+        mpdclient = RaspiGPIOMPDClient()
+        mpdclient.initLogging(log)
 
-    if not mpdclient.readConfigFile():
-        sys.exit(-2)
+        if not mpdclient.readConfigFile():
+            sys.exit(-2)
 
-    if not mpdclient.config["GPIO"]:
-        log.error("Invalid configuration file! (No [GPIO] section)")
-        sys.exit(-3)
+        if not mpdclient.config["GPIO"]:
+            log.error("Invalid configuration file! (section [GPIO] missing)")
+            sys.exit(-3)
 
-    if not mpdclient.initMPD():
-        log.error("Init MPD connection failed!")
-        sys.exit(-4)
+        if not mpdclient.initGPIO():
+            log.error("Init GPIO failed!")
+            sys.exit(-3)
 
-    if not mpdclient.initGPIO():
-        log.error("Init GPIO failed!")
-        sys.exit(-3)
+        if not mpdclient.initMPD():
+            log.error("Init MPD connection failed!")
+            sys.exit(-4)
 
-    log.info("Enter raspi-gpio-mpdc service loop...")
-    while True:
-        time.sleep(1)
+        if not mpdclient.isConnected:
+            if not mpdclient.connectMPD():
+                log.error(
+                    "Could not connect to MPD server - timed out after 50 attempts!"
+                )
+                sys.exit(-5)
 
-except Exception as e:
-    if log:
-        log.exception(f"Unhandled exception: {e}")
-    sys.exit(-1)
-finally:
-    if mpdclient and mpdclient.isConnected:
+        log.info("Enter raspi-gpio-mpdc service loop...")
+        while True:
+            time.sleep(0.1)
+
+    except Exception as e:
         if log:
-            log.info("Disconnect from MPD.")
-        mpdclient.mpd.disconnect()
+            log.exception(f"Unhandled exception: {e}")
+        sys.exit(-1)
+    finally:
+        if mpdclient and mpdclient.isConnected:
+            if log:
+                log.info("Disconnect from MPD.")
+            mpdclient.mpd.disconnect()
+
+
+# run as script only
+if __name__ == "__main__":
+    main()
